db65cf059cf733e08690df559f5f65aa0808b9a6
Polidoro-root <jv.polidoro@outlook.com>
2023-10-30 23:53:12 -0300

    linked_list sentinels

--- chapter10_elementary_data_structures/linked_list.c
+++ chapter10_elementary_data_structures/linked_list.c
@@ -16,6 +16,22 @@ typedef struct {
   uint8_t key;
 } doubly_linked_list;
 
+typedef struct list_sentinel_nil list_sentinel_nil;
+
+struct list_sentinel_nil {
+  uint8_t key;
+  list_sentinel_nil *prev;
+  list_sentinel_nil *next;
+};
+typedef struct list_sentinel list_sentinel;
+
+struct list_sentinel {
+  uint8_t key;
+  list_sentinel *prev;
+  list_sentinel *next;
+  list_sentinel_nil *nil; // next = head; prev = tail;
+};
+
 list_item *listsearch(doubly_linked_list *l, uint8_t key) {
   list_item *x = l->head;
 
@@ -57,5 +73,37 @@ void listdelete(doubly_linked_list *l, list_item *x) {
 
   if (x->next != NULL) {
     x->next->prev = x->prev;
+  }
+}
+
+void listdeletesentinel(list_sentinel *x) {
+  x->prev->next = x->next;
+  x->next->prev = x->prev;
+}
+
+void listinsertsentinel(list_sentinel *x, list_sentinel *y) {
+  x->next = y->next;
+  x->prev = y;
+  x->next->prev = x;
+  y->next = x;
+}
+
+list_sentinel_nil *listsearchsentinel(list_sentinel *l, uint8_t key) {
+  list_sentinel_nil *x;
+
+  x = malloc(sizeof(list_sentinel_nil));
+
+  l->nil->key = key; // store the key in the sentinel to guarantee it is in list
+
+  x = l->nil->next; // start at the head of the list
+
+  while (x->key != key) {
+    x = x->next;
+  }
+
+  if (x == l->nil) {
+    return NULL;
+  } else {
+    return x;
   }
 }
